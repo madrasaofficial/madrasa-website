0dfe64d167b1cd79b25a23820e5c1683f0492c8e
a-sad-dan <danishasad91@gmail.com>
2024-12-17 14:53:01 +0530

    Implemented tablet mode, fixed FAQ animations, design

--- src/main.ts
+++ src/main.ts
@@ -53,11 +53,11 @@ function renderFAQs(category: string) {
   // Create FAQ items
   filteredFAQs.forEach((faq) => {
     const faqItem = document.createElement("div");
-    faqItem.classList.add("faq-item");
+    faqItem.classList.add("faq-item","bg-gray-100","rounded-xl");
     faqItem.innerHTML = `
-    <div class="p-4 px-5 bg-gray-100 rounded-lg">
-        <div class="flex gap-2 items-top justify-between cursor-pointer">
-          <h3 class="text-neutral-800 font-semibold text-left w-11/12">${faq.question}</h3>
+    <div class="p-4 px-5">
+        <div class="flex gap-2 items-center justify-between cursor-pointer">
+          <h3 class="faq-question text-neutral-800 font-semibold text-left w-11/12">${faq.question}</h3>
           <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="shrink-0">
             <path d="M9.99999 4.1665V15.8332M4.16666 9.99984H15.8333" stroke="#111024" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
@@ -80,12 +80,16 @@ function renderFAQs(category: string) {
       faqTitle.addEventListener("click", () => {
         faqAnswer.classList.toggle("open");
         faqSvg.classList.toggle("rotate-45");
+        faqTitle.classList.toggle("text-violet-800");
+        faqItem.classList.toggle("bg-violet-50");
       });
 
       // Optionally, toggle the SVG for open/close
       faqSvg.addEventListener("click", () => {
         faqAnswer.classList.toggle("open");
         faqSvg.classList.toggle("rotate-45");
+        faqTitle.classList.toggle("text-violet-800");
+        faqItem.classList.toggle("bg-violet-50");
       });
     }
   });
